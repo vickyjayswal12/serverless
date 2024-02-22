d6a0d8ce8e263b3046eb907c7059c673615c73f3
vickyjayswal12 <vickyjayswal12@gmail.com>
2024-02-22 18:46:32 +0530

    added aws cognito for register,login and authorisation for profile path

--- handler.js
+++ handler.js
@@ -1,93 +1,116 @@
-// const AWS = require('aws-sdk');
-// const { v4: uuidv4 } = require('uuid');
-
-// const cognito = new AWS.CognitoIdentityServiceProvider();
-// const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-// const TABLE_NAME = 'UserProfiles';
-
 require('dotenv').config();
-
-var AWS = require('aws-sdk');
-
-const my_AWSAccessKeyId = process.env.AWSAccessKeyId;
-const my_AWSSecretKey = process.env.AWSSecretKey;
-const aws_region = process.env.region;
-const empTable = process.env.tableName;
-
-var dynamoDB = new AWS.DynamoDB.DocumentClient({
-    region: aws_region,
-    accessKeyId: my_AWSAccessKeyId,
-    secretAccessKey: my_AWSSecretKey,
-});
+const AWS = require('aws-sdk');
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 module.exports.register = async (event) => {
-   const {id,name,email, password } = JSON.parse(event.body);
-  var params = {
-    TableName:empTable,
-    Item: {
-        id: id,
-        name:name,
-        email:email,
-        password:password
-    },
-    ReturnValues: 'ALL_OLD' 
-};
-
-let putItem = new Promise((res, rej) => {
-    dynamoDB.put(params, function(err, data) {
-        if (err) {
-            console.log("Error", err);
-            rej(err);
-        } else {
-            console.log("Success!");
-            res(data);
-        }
-    }); 
-});
-const res = await putItem;
-console.log(res);  
- 
+  const { email, password } = JSON.parse(event.body); //password should be string
+  
+  try {
+    const params = {
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        { Name: 'email', Value: email }
+      ]
+    };
+    await cognitoidentityserviceprovider.signUp(params).promise();
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "register successfully",result:res }),
+      body: JSON.stringify({ message: 'User successfully registered' })
     };
-  // }
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ message: error.message || 'An error occurred' })
+    };
+  }
 };
 
+module.exports.verifyCode = async (event) => {
+  const { email, code } = JSON.parse(event.body);  //code should be string
+  
+  try {
+    const params = {
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      Username: email,
+      ConfirmationCode: code
+    };
+    await cognitoidentityserviceprovider.confirmSignUp(params).promise();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'User successfully verified' })
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ message: error.message || 'An error occurred' })
+    };
+  }
+};
+
+
 module.exports.login = async (event) => {
- 
-  return {
-    statusCode: 500,
-    body: JSON.stringify({ message: "login",body:JSON.parse(event.body) }),
-  };
+  const { email, password } = JSON.parse(event.body);
+
+  try {
+    const params = {
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      AuthParameters: {
+        'USERNAME': email,
+        'PASSWORD': password
+      }
+    };
+    const data = await cognitoidentityserviceprovider.initiateAuth(params).promise();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ accessToken: data.AuthenticationResult.AccessToken })
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ message: error.message || 'An error occurred' })
+    };
+  }
 };
 
 module.exports.profile = async (event) => {
-  // const id = event.pathParameters.userId
-  console.log("params",event.pathParameters,typeof(event.pathParameters.id));
-    const params = {
-        TableName:empTable,
-        Key:{
-            id: Number(event.pathParameters.id)
-        }
+  try {
+    const { sub: userId } = event.requestContext.authorizer.claims;
+    const user = await getUserProfile(userId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(user)
     };
-
-    let queryExecute = new Promise((res, rej) => {
-        dynamoDB.get(params, function(err, data) {
-            if (err) {
-                console.log("Error", err);
-                rej(err);
-            } else {
-                console.log("Success! get method fetch data from dynamodb");
-                res(JSON.stringify(data, null, 2));
-            }
-        }); 
-    });
-
-    const res=await queryExecute;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "profile",result:JSON.stringify(res) }),
-  };
+  } catch (error) {
+    return {
+      statusCode: 401, // Unauthorized
+      body: JSON.stringify({ message: 'Unauthorized' })
+    };
+  }
 };
+
+async function getUserProfile(userId) {
+  const params = {
+    UserPoolId: process.env.COGNITO_USER_POOL_ID,
+    Filter: `sub = "${userId}"`
+  };
+
+  const data = await cognitoidentityserviceprovider.listUsers(params).promise();
+
+  if (data.Users.length === 0) {
+    throw new Error('User not found');
+  }
+
+  const user = {
+    email: data.Users[0].Username,
+    // Add additional attributes as needed
+  };
+
+  return user;
+}
